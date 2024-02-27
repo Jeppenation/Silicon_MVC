a6d7575efc058d7e17a2956585f2cf59a9bf8821
jesper kallioniemi <hwilajesper@live.com>
2024-02-27 12:49:31 +0100

    Bin-files

--- SiliconMVC/obj/Debug/net8.0/SiliconMVC.AssemblyInfo.cs
+++ SiliconMVC/obj/Debug/net8.0/SiliconMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiliconMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd8e7780a5ac1051fda746ea88573d63fd4482b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiliconMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiliconMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
